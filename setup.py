b9913741eef07d787e19c195ce0e5125d86fc817
Michel Gutmann <mich.gutmann@gmail.com>
2021-05-18 22:51:33 +0200

    Prepares package for upload to PyPi

--- setup.py
+++ setup.py
@@ -4,12 +4,20 @@ with open("README.md", 'r') as f:
     long_description = f.read()
 
 setup(
-   name='mep_api',
+   name='mep_api-mgutmann',
    version='0.1',
    description='A web scraping package for data on members of the European Parliament.',
    license="MIT",
    long_description=long_description,
+   long_description_content_type="text/markdown",
    author='Michel Gutmann',
+   author_email="mich.gutmann@gmail.com",
    url="http://www.github.com/mgutmann/mep_api",
+   classifier=[
+	"Programming Language :: Python :: 3",
+	"Licence :: OSI Approved :: MIT License",
+	"Operating System :: OS Independent",
+   ],
+   python_requires=">=3.6",
    install_requires=['requests', 'beautifulsoup4'], #external packages as dependencies
 )
